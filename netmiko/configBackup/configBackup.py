5e21089e8692d461ed18a472e4bcc7271bd9a230
AppleCIDR <danielgcass@protonmail.com>
2025-10-14 11:10:42 -0400

    Update configBackup.py

--- netmiko/configBackup/configBackup.py
+++ netmiko/configBackup/configBackup.py
@@ -1,1 +1,22 @@
+import os
+from netmiko import ConnectHandler
+from dotenv import load_dotenv
 
+load_dotenv()
+switchPass = os.getenv("SWITCH_PASS")
+
+switch1 = {
+    'device_type': 'aruba_aoscx',
+    'host':   'switchIP',
+    'username': 'manager',
+    'password': switchPass,
+    'port' : 22,
+}
+
+net_connect = ConnectHandler(**switch1)
+
+hostname = net_connect.send_command('show hostname')
+config_commands = [ 'copy running-config tftp://tftpServer/' + hostname +' cli' ]
+
+output = net_connect.send_config_set(config_commands)
+print(output)
